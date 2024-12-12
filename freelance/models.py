b585fe1036f61179edbdfd61821bb47f797b3d0d
Adam <adamdoxaev5@gmail.com>
2024-12-12 22:05:35 +0300

    added seedizers, fixed the bug

--- freelance/models.py
+++ freelance/models.py
@@ -1,138 +1,131 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# исполнитель
+# Create your models here.
 class Executor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=11)
 
     def __str__(self):
-        return f"{self.user}, phone: {self.phone}"
+        return "User: {}, phone: {}".format(self.user, self.phone)
 
 
-
-# заказчик
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=11)
 
     def __str__(self):
-        return f"{self.user}, phone: {self.phone}"
+        return "User: {}, phone: {}".format(self.user, self.phone)
 
 
-# сервисы с описканием услиги и исполнителями, которые могут выаолнить задачу
 class Service(models.Model):
     SERVICE_TYPES = [
-        ("1", "Веб разработка"),
-        ("2", "Маркетинг"),
-        ("3", "Копирайтинг"),
-        ("4", "Рерайтинг"),
-        ("5", "Переводы"),
-        ("6", "Видеомонтаж"),
-        ("7", "Фотография"),
-    ]
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE)
+                ('1', 'Веб разработка'),
+                ('2', 'Маркетинг'),
+                ('3', 'Копирайтинг'),
+                ('4', 'Рерайтиинг'),
+                ('5', 'Переводы'),
+                ('6', 'Видеомонтаж'),
+                ('7', 'Фотография')
+            ]
+
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     desc = models.CharField(max_length=1000)
     price = models.IntegerField()
-    service_type = models.CharField(choices=SERVICE_TYPES, default="1", max_length=1)
+    service_type = models.CharField(choices=SERVICE_TYPES, default='1', max_length=1)
 
     def __str__(self):
-        return f"{self.name}, {self.get_service_type_display()}, price: {self.price}"
+        return '{}, {}, price: {}'.format(self.name, self.get_service_type_display(), self.price)
 
 
-
-# заказ
 class Order(models.Model):
-    SERVICE_TYPES = [
-        ("1", "Веб разработка"),
-        ("2", "Маркетинг"),
-        ("3", "Копирайтинг"),
-        ("4", "Рерайтинг"),
-        ("5", "Переводы"),
-        ("6", "Видеомонтаж"),
-        ("7", "Фотография"),
-    ]
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE)
+    ORDER_TYPES = [
+                ('1', 'Веб разработка'),
+                ('2', 'Маркетинг'),
+                ('3', 'Копирайтинг'),
+                ('4', 'Рерайтиинг'),
+                ('5', 'Переводы'),
+                ('6', 'Видеомонтаж'),
+                ('7', 'Фотография')
+            ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     desc = models.CharField(max_length=1000)
     price = models.IntegerField()
-    service_type = models.CharField(choices=SERVICE_TYPES, default="1", max_length=1)
+    order_type = models.CharField(choices=ORDER_TYPES, default='1', max_length=1)
 
     def __str__(self):
-        return f"{self.name}, {self.get_service_type_display()}, price: {self.price}"
+        return '{}, {}, price: {}'.format(self.name, self.get_order_type_display(), self.price)
 
 
-# Тэги (похоже на категории)
 class Tag(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE, blank=True, null=True)
-    order = models.ForeignKey("Order", on_delete=models.CASCADE, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return '{}'.format(self.name)
 
-# Выполнение заказа
+
 class Ordering(models.Model):
-    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE, blank=True, null=True)
-    order = models.ForeignKey("Order", on_delete=models.CASCADE, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
     order_date = models.DateTimeField()
     deadline = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.order_date} - {self.deadline}, Customer {self.customer}, Executor: {self.executor}"
+        return '{} - {}, Customer: {}, Executor: {}'.format(self.order_date, self.deadline, self.customer, self.executor)
 
 
-# коментарии (возможно)
 class Message(models.Model):
-    customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE)
-    msq_date = models.DateTimeField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE)
+    msg_date = models.DateTimeField()
     is_edited = models.BooleanField(default=False)
     desc = models.CharField(max_length=1000)
 
 
-# техподдержка
 class Ticket(models.Model):
-
-    # Важность проблемы
     SEVERITIES = [
-            ("1", "Низкая"),
-            ("2", "Средняя"),
-            ("3", "Высокая"),
-        ]
-    customer = models.ForeignKey("Customer", on_delete=models.CASCADE, blank=True, null=True)
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE, blank=True, null=True)
-    severity = models.CharField(choices=SEVERITIES, default="1", max_length=1)
+                ('1', 'Низкая'),
+                ('2', 'Средняя'),
+                ('3', 'Высокая')
+            ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True)
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE, blank=True, null=True)
+    severity = models.CharField(choices=SEVERITIES, default='1', max_length=1)
     desc = models.CharField(max_length=1000)
-    is_resolved = models.BooleanField(default=False) # решена/не решена проблема
+    ticket_date = models.DateTimeField()
+    is_resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{}, {}, Is resolved? {}'.format(self.get_severity_display(), self.ticket_date, self.is_resolved)
 
 
-
-
-# позволяет пользователям оставлять отзывы о выполненных услугах.
 class Review(models.Model):
-    RAITING_FIELD = [
-        ("1", "1"),
-        ("2", "2"),
-        ("3", "3"),
-        ("4", "4"),
-        ("5", "5"),
-    ]
-    rating = models.CharField(choices=RAITING_FIELD, default="1", max_length=1)
+    RATING_FILLED = [
+                ('1', 1),
+                ('2', 2),
+                ('3', 3),
+                ('4', 4),
+                ('5', 5)
+            ]
+
+    rating = models.CharField(choices=RATING_FILLED, default='1', max_length=1)
     desc = models.CharField(max_length=1000)
 
 
-# связывание отзывов с конкретными пользователями (авторами отзывов),
-# а также с заказчиками и исполнителями, которые были вовлечены в процесс выполнения услуги.
 class Authoring(models.Model):
-    review = models.ForeignKey("Review", on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    customer = models.ForeignKey("Customer", on_delete=models.CASCADE, blank=True, null=True)
-    executor = models.ForeignKey("Executor", on_delete=models.CASCADE, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True)
+    executor = models.ForeignKey(Executor, on_delete=models.CASCADE, blank=True, null=True)
     review_date = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.author}, {self.review_date}"
-
+        return '{}, {}'.format(self.author, self.review_date)
