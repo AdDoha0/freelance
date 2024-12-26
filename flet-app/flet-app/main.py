ae9f986d19d6f0d9d8f7a35501e9275be9e769b3
Adam <adamdoxaev5@gmail.com>
2024-12-26 21:03:04 +0300

    once again I COMPLETELY redesigned the routing and moved the navbar to the main.py file

--- flet-app/flet-app/main.py
+++ flet-app/flet-app/main.py
@@ -1,5 +1,5 @@
 import flet as ft
-
+from flet import TemplateRoute
 
 from components.important_details import create_navbar
 
@@ -12,52 +12,110 @@ from screens.create_order_form_screen import CreateOrderPage
 from screens.open_order_screens import OpenOrderPage
 
 
-
-
-
 def main(page: ft.Page):
     # настройки страницы
-    page.window_width = 370
-    page.window_height = 750
+    page.window.width = 370
+    page.window.height = 750
     page.theme_mode = "dark"
     page.bgcolor = "#1E1E2F"
     page.padding = 12
     page.title = "Фриланс Приложение"
 
-
     page.appbar = ft.AppBar(
-        title=ft.Text("Логотип", size=20, weight=ft.FontWeight.BOLD, color="#B2FF66"),  # Логотип в виде текста
-        center_title=True,  # Центрирование заголовка
-        bgcolor="#1E1E2F",  # Цвет фона AppBar
+        title=ft.Text("Логотип", size=20, weight=ft.FontWeight.BOLD,
+                      color="#B2FF66"),
+        center_title=True,
+        bgcolor="#1E1E2F",
     )
 
-    pages = {
-            "home": HomePage(page),
-            "cat_or_search": CatOrSearcPage(page),
-            "login": LoginPage(page),
-            "authorization": AuthorizationPage(page),
-            "profile": ProfilePage(page),
-            "create_order": CreateOrderPage(page),
-            "open_order": OpenOrderPage(page),
-        }
+    def handle_route_change(route: str):
+        page.clean()
 
-    def switch_page(page_name):
-        page.clean()  # Очищаем текущую страницу
-        page.add(pages[page_name].build())  # Добавляем новую страницу
+        troute = TemplateRoute(route)
+
+        if troute.match("/home"):
+            home_page = HomePage(page)
+            page.add(home_page.build())
+
+        elif troute.match("/auth/login"):
+            login_page = LoginPage(page)
+            page.add(login_page.build())
+        elif troute.match("/auth/authorization"):
+            authorization_page = AuthorizationPage(page)
+            page.add(authorization_page.build())
+
+        elif troute.match("/search"):
+            search_page = CatOrSearcPage(page)
+            page.add(search_page.build())
+
+        elif troute.match("/order/create_order"):
+            create_order_page = CreateOrderPage(page)
+            page.add(create_order_page.build())
+        elif troute.match("/order"):
+            open_order_page = OpenOrderPage(page)
+            page.add(open_order_page.build())
+        elif troute.match("/profile"):
+            profile_page = ProfilePage(page)
+            page.add(profile_page.build())
+        else:
+            page.add(ft.Text("404 - Page not found"))
+
         page.add(create_navbar(switch_page))
 
+    def switch_page(route: str):
+        page.route = route
+        handle_route_change(route)
 
-    page.on_route_change = switch_page
 
 
-    # Добавляем первую страницу
-    switch_page("home")
+
+    def create_navbar(switch_page) -> ft.Row:
+        navbar = ft.Row(
+            controls=[
+                ft.IconButton(
+                    ft.icons.HOME,
+                    tooltip="Главная",
+                    icon_color="#B2FF66",
+                    on_click=lambda e: switch_page("/home")
+                ),
+                ft.IconButton(
+                    ft.icons.SEARCH_SHARP,
+                    tooltip="Избранное",
+                    icon_color="#B2FF66",
+                    on_click=lambda e: switch_page("/search")
+                ),
+                ft.IconButton(
+                    ft.icons.CONTACT_MAIL,
+                    tooltip="Контакты",
+                    icon_color="#B2FF66",
+                    on_click=lambda e: switch_page("/profile")
+                ),
+                ft.IconButton(
+                    ft.icons.WARNING,
+                    tooltip="Тестовый лог",
+                    icon_color="#B2FF66",
+                    on_click=lambda e: switch_page("/auth/login")
+                ),
+                ft.IconButton(
+                    ft.icons.WARNING,
+                    tooltip="Тестовый лог",
+                    icon_color="#B2FF66",
+                    on_click=lambda e: switch_page("/auth/authorization")
+                ),
+
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True
+        )
+        return ft.Container(
+            content=navbar,
+            bgcolor="#1A1A2E",
+            padding=ft.padding.only(top=20, bottom=15),
+            margin=ft.margin.only(left=-12, right=-12, top=-12, bottom=-12)
+        )
+
+    switch_page("/home")
 
 if __name__ == "__main__":
     ft.app(target=main)
-
-
-
-
-
-
